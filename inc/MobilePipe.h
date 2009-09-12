daf32dbd260f3a22846458bd70c4c7401a0217b2
unknown <bill@.(none)>
2009-09-11 17:23:19 -0700

    Initial Commit!!!

---
+++ inc/MobilePipe.h
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name		: MobilePipe.h
+ Author	  : 
+ Copyright   : Your copyright notice
+ Description : Exe header file
+ ============================================================================
+ */
+
+#ifndef __MOBILEPIPE_H__
+#define __MOBILEPIPE_H__
+
+//  Include Files
+
+#include <e32base.h>
+
+//  Function Prototypes
+
+GLDEF_C TInt E32Main();
+
+#endif  // __MOBILEPIPE_H__
+
